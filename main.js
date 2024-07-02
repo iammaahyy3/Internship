cf953b8c0976674afca425a595c9a7a43bade76c
Mahi Chuhan <mahichauhan.logicseed@gmail.com>
2024-07-02 15:17:10 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let x = myfunction(3,4,12)
+
+function myfunction(a,b,c)
+{
+    return(a*b*c)
+}
+console.log(x)
+
+function greeting()
+{
+    console.alert('Hello!');
+}
+
+/* 
+
+const v = totalSum(56,78)
+
+function totalSum()
+{
+    return(a*b*c)
+}
+
+console.log(v) */
+
